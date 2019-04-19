c7f05e9c827f7c98e887415e9b2e45a0899e73df
Louis Alcorn <loualcorn@utexas.edu>
2019-04-19 12:48:26 -0500

    working on it with smaller geography testing (passadena, TX within Harris county)

--- 05-walking-distance-nearest-stop-pre-postSR.R
+++ 05-walking-distance-nearest-stop-pre-postSR.R
@@ -10,18 +10,25 @@ library(tidyverse)
 library(maptools)
 census_api_key("3a277651e6ec078927e10356269269b3698a0cfa")
 
-#pull down entire street network from open street map using dodgr package (WARNING: Takes a few minutes)
-harriscty <- dodgr_streetnet("harris county texas")
-#weight the street network for walking to prevent routes from allowing walk paths along limited-access freeways
-graph <- weight_streetnet(harriscty, wt_profile = "foot", type_col = "highway",
+##pull down entire street network from open street map using dodgr package (WARNING: Takes a few minutes)
+#harriscty <- dodgr_streetnet("harris county texas")
+pasadenatest <-dodgr_streetnet("pasadena texas")
+##weight the street network for walking to prevent routes from allowing walk paths along limited-access freeways
+#graph <- weight_streetnet(harriscty, wt_profile = "foot", type_col = "highway",
+#                          id_col = "osm_id", keep_cols = NULL)
+graphtest <- weight_streetnet(pasadenatest, wt_profile = "foot", type_col = "highway",
                           id_col = "osm_id", keep_cols = NULL)
 
-#convert dodgr_streetnet file to sf.tibble (WARNING: Takes a long time)
-graph_sf <- dodgr_to_sf(graph) #linestring xy WGS84-4326 w/ 1159768 features and 13 fields
-#convert to crs (NAD83 / Texas Central with UOM = meters for later processing steps)
-graph_sf_t <- st_transform(graph_sf, 32139) #linestring xy 32139 w/ 1159769 features and 13 fields
+##convert dodgr_streetnet file to sf.tibble (WARNING: Takes a long time)
+#graph_sf <- dodgr_to_sf(graph) #linestring xy WGS84-4326 w/ 1159768 features and 13 fields
+graphtest_sf <- dodgr_to_sf(graphtest) #linestring xy WGS84-4326 w/ 1159768 features and 13 fields
 
-#Get Block Group data from ACS for folllwing demographic groups (summary variable is total pop):
+##convert to crs (NAD83 / Texas Central with UOM = meters for later processing steps)
+#graph_sf_t <- st_transform(graph_sf, 32139) #linestring xy 32139 w/ 1159769 features and 13 fields
+graphtest_sf_t <- st_transform(graphtest_sf, 32139) #linestring xy 32139 w/ 1159769 features and 13 fields
+
+
+##Get Block Group data from ACS for folllwing demographic groups (summary variable is total pop):
 hlstatusvars <- c("B03002_003", # white alone
                   "B03002_004", # black alone
                   "B03002_006", # Asian
@@ -39,60 +46,48 @@ harris_bg <- get_acs(geography = "block group", variables = hlstatusvars,
 #                     summary_var = "B03002_001") 
 #harris_tract
 
-#transform ACS data to centroid of block groups to create 'origins' for analysis
+##transform ACS data to centroid of block groups to create 'origins' for analysis
 harris_bg_cent <- st_centroid(harris_bg)
-#transform bg centroids to wgs84 to be consistent crs with graph streetnet for dodgr calcs.
+##transform bg centroids to wgs84 to be consistent crs with graph streetnet for dodgr calcs.
 harris_bg_cent_wgs <- st_transform(harris_bg_cent, 4326)
 harris_bg_cent_nad83 <- st_transform(harris_bg_cent, 32139)
-
-#snap bg centroids to closest link on street network
+##snap bg centroids to closest link on street network
 harris_bg_cent_wgs_snap <- snapPointsToLines(harris_bg_cent_wgs, graph_sf, maxDist = 400, withAttrs = TRUE)
 ###returns the following:[ Error in (function (classes, fdef, mtable)  : 
 ###unable to find an inherited method for function ‘is.projected’ for signature ‘"sf"’]
 
-#snap bg centroids to closest node on street network (backup since above didn't work)
-#(WARNING... the following command takes more than 2 DAYS!...)
-harris_bg_cent_wgs_snap <- st_snap(harris_bg_cent_nad83, graph_sf_t, tolerance = 400)
-#previous is actually in nad83 (meters). Following command converts back to wgs84 lat long
+##snap bg centroids to closest node on street network (backup since above didn't work)
+##(WARNING... the following command takes more than 2 DAYS!...)
+##(WARNING...test function takes approximately 45 mins)
+harris_bg_cent_wgs_snap <- st_snap(harris_bg_cent_nad83, graphtest_sf_t, tolerance = 400)
+pre_merged_wgs_snap <- st_snap(pre_merged_t, graphtest_sf_t, tolerance = 400)
+post_merged_wgs_snap <- st_snap(post_merged_t, graphtest_sf_t, tolerance = 400)
+##previous is actually in nad83 (meters). Following command converts back to wgs84 lat long
 harris_bg_cent_wgs_snap_t <- st_transform(harris_bg_cent_wgs_snap, 4326)
-#fromsnap <- st_snap(harris_tract_c_t, graph_sf_t, tolerance = 99999999)
-#fromsnap_t <- st_transform(fromsnap, 4269)
-#fromsnap_t
-#nrow(graph)
+pre_merged_wgs_snap_t <- st_transform(pre_merged_wgs_snap, 4326)
+post_merged_wgs_snap_t <- st_transform(post_merged_wgs_snap, 4326)
 
-
-#harris_tract_t <- st_transform(harris_tract, 4269)
-#pre_stops_t <- st_transform(pre_merged , 4269)
-#post_stops_t <- st_transform(post_merged , 4269)
-#graph
-#harris_tract_c
-#harris_tract_c <- st_centroid(harris_tract_t)
-
-#setting to (bus stops) and from (bg centroids) snapped to street network (graph) for the dodgr path matrix
-#testing from and to as same set of points first
+##setting to (bus stops) and from (bg centroids) snapped to street network (graph) for the dodgr path matrix
 from <- harris_bg_cent_wgs_snap_t
-to_pre <- harris_bg_cent_wgs_snap_t
-to_post <- harris_bg_cent_wgs_snap_t
+to_pre <- pre_merged_wgs_snap_t
+to_post <- post_merged_wgs_snap_t
+##converting to xy vector to pass through dodgr_dists function
+from_crs <- st_coordinates(from)
+to_pre_crs <- st_coordinates(to_pre)
+to_post_crs <- st_coordinates(to_post)
 
-#from$latlon <- from$geometry
-#to_pre$latlon <- to_pre$geometry
-#to_post$latlon <- to_post$geometry
-#from_sc <- fromsnap_t %>%
-#  mutate(fromx = unlist(map(fromsnap_t$geometry, 1)),
-#         fromy = unlist(map(fromsnap_t$geometry, 2)))
-#to_pre_sc <- to_pre %>%
-#  mutate(fromx = unlist(map(to_pre$geometry, 1)),
-#         fromy = unlist(map(to_pre$geometry, 2)))
-#from <- from_sc
-#to <- to_pre_sc
-#from_sampletest <- sample(graph$from_id, size = 100)
-#to_sampletest <- sample(graph$to_id, size = 50)
-
-walkdist_preSR <- dodgr_dists(graph, from, to_pre, wt_profile = "foot", expand = 0, 
+##generating walking distnace matrix between from/to (note this is a test because from and to are same)
+##WARNING...the following command takes XXX minutes)
+walkdist_preSR <- dodgr_dists(graphtest, from_crs, to_pre_crs, wt_profile = "foot", expand = 0, 
                               heap = "BHeap", parallel = TRUE, quiet = TRUE)
-
-walkdist_postSR <- dodgr_dists(graph, from, to_post, wt_profile = "foot", expand = 0, 
+write.csv(walkdist_preSR, file = "walkdist_preSR_distmatrix.csv")
+walkdist_postSR <- dodgr_dists(graph, from_crs, to_post_crs, wt_profile = "foot", expand = 0, 
                               heap = "BHeap", parallel = TRUE, quiet = TRUE)
-walkdist_preSR
+write.csv(walkdist_postSR, file = "walkdist_postSR_distmatrix.csv")
 
-
+write.csv(harris_bg_cent, file = "harris_bg_cent_ref.csv")
+write.csv(pre_merged, file "preSR_stops.csv")
+write.csv(post_merged, file "postSR_stops.csv")
+write.csv(harris_bg_cent_wgs_snap_t, file = "harris_bg_snapref.csv")
+write.csv(pre_merged_wgs_snap_t, file = "pre_SR_stops_snapref.csv")
+write.csv(post_merged_wgs_snap_t, file = "post_SR_stops_snapref.csv")
